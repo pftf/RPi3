f386249c1d82bd9f25b1b102ddb6a4b229dd0ddd
Pete Batard <pete@akeo.ie>
2019-07-19 21:27:51 +0100

    fix missing GCC5_AARCH64_PREFIX definition

--- build_firmware.sh
+++ build_firmware.sh
@@ -1,6 +1,5 @@
-﻿#!/bin/sh
-export CROSS_COMPILE=/usr/bin/aarch64-linux-gnu-
+﻿export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
 export WORKSPACE=$PWD
 export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms:$WORKSPACE/edk2-non-osi
-. edk2/edksetup.sh
+source edk2/edksetup.sh
 build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/RaspberryPi/RPi3/RPi3.dsc -b RELEASE --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVersionString=L"edk2-$FIRMWARE_VERSION"
