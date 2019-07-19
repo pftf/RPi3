50ea1a467a89b22afb239c19b69679d789dcc415
Pete Batard <pete@akeo.ie>
2019-07-19 21:39:28 +0100

    try direct again

--- build_firmware.sh
+++
@@ -1,5 +0,0 @@
-﻿export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
-export WORKSPACE=$PWD
-export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms:$WORKSPACE/edk2-non-osi
-source edk2/edksetup.sh
-build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/RaspberryPi/RPi3/RPi3.dsc -b RELEASE --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVersionString=L"edk2-$FIRMWARE_VERSION"
