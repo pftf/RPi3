4a6f466167b42e0552d566700d2212b0ad3f7fa7
Pete Batard <pete@akeo.ie>
2019-09-16 19:29:08 +0100

    Update to EDK2 latest

--- build_firmware.sh
+++ build_firmware.sh
@@ -5,4 +5,4 @@ export WORKSPACE=$PWD
 export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms:$WORKSPACE/edk2-non-osi
 source edk2/edksetup.sh
 build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/RaspberryPi/RPi3/RPi3.dsc -b DEBUG -D SECURE_BOOT_ENABLE=TRUE --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVersionString=L"UEFI Firmware $APPVEYOR_REPO_TAG_NAME (DEBUG)"
-build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/RaspberryPi/RPi3/RPi3.dsc -b RELEASE -D SECURE_BOOT_ENABLE=TRUE --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVersionString=L"UEFI Firmware $APPVEYOR_REPO_TAG_NAME (EDK2)"
+build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/RaspberryPi/RPi3/RPi3.dsc -b RELEASE -D SECURE_BOOT_ENABLE=TRUE --pcd gEfiMdeModulePkgTokenSpaceGuid.PcdFirmwareVersionString=L"UEFI Firmware $APPVEYOR_REPO_TAG_NAME"
